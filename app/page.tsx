edcbcc194b40e3a004066519e6ba410b88c5f6e2
tchi-sama <tchisama41@gmail.com>
2023-07-07 11:06:45 +0200

    text white

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <main className="bg-white relative hero min-h-screen flex flex-col items-center justify-center">
       <Navbar/>
-      <h1 className='text-[18vw] md:text-[16vw] drop-shadow-2xl rotate-12 skew-x-12 font-bold uppercase'>Tchisama</h1>
+      <h1 className='text-[18vw] text-white md:text-[16vw] drop-shadow-2xl rotate-12 skew-x-12 font-bold uppercase'>Tchisama</h1>
       <Image src={me} alt='' className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[65%] object-contain'></Image>
       <div className='absolute container mx-auto flex flex-col gap-4 bottom-0 md:bottom-[8%] left-0 p-4 md:p-10 items-start'>
-        <p className='text-lg font-medium uppercase w-full max-w-sm'>a full stack web devloper , and designer based on morocco</p>
+        <p className='text-lg text-white font-medium uppercase w-full max-w-sm'>a full stack web devloper , and designer based on morocco</p>
         <button className='btn bg-white border-none text-[#543D25] hover:bg-gray-100 hover:text-[#543D25]'>
           lets talk 
           <ArrowForward/>
